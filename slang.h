5f9bab6df1fb6f5be4c197c2ab1aaaf4c2a56fde
u0024 lang <u0024lang@gmail.com>
2013-04-12 10:59:10 -0500

    initial commit

---
+++ slang.h
@@ -0,0 +1,38 @@
+// types
+#define digits          int
+#define big_numba       float
+#define reely_big_numba double
+#define letta           char
+#define yesano          bool
+
+
+// operators
+#define is         =
+#define an         +
+#define less       -
+#define divvy      /
+#define less_den   <
+#define mo_den    >
+#define andone     ++
+
+// booleans
+#define i_tol_ya  true
+#define yo_liar   false
+
+// control flow
+#define fo          for     /* fo (i is 0; i lessden lim; i andone) { ... } */
+#define wa          while   /* do { ... } wa (cond); */
+#define til         while  /* til (cond) { ... }  */
+#define if_he_lyin  else if
+
+
+// numbers
+#define nuttin  0
+
+// IO
+#define ask      cin >>
+#define say      cout <<
+
+
+
+
---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "slang.h"
+
+using namespace std;
+
+digits main(digits argc, letta ** argv) {
+	digits total is nuttin;
+	digits num is 5;
+
+	fo(digits i is nuttin; i less_den num; i andone) {
+		total is total an i;
+	}
+
+	big_numba average is total divvy num;
+
+	say "total is ";
+	say total;
+
+	say "\naverage is ";
+	say average;
+
+}
